138d57e22b37fca5a3a0f68d40f09a29b6483ec3
Ricky JW <rickywid@hotmail.com>
2017-02-07 11:59:49 -0500

    update bundle.js

--- src/components/project.js
+++ src/components/project.js
@@ -1,13 +1,11 @@
 import React, { Component } from 'react';
 
-
-
 export default class Project extends Component {
 	
 	render() {
 
-		const lrgImg = "../../style/img/portfolio/lg";
-		const smImg = "../../style/img/portfolio/sm";
+		const lrgImg = "style/img/portfolio/lg";
+		const smImg = "style/img/portfolio/sm";
 		return (
 			<div className="projects__project" onClick={this.props.showModal.bind(this, this.props.id)} >
 				<a className="projects__project-link" href="#">
